fbc35900b15b2be7a238e30b2be234c7c57f04f0
pfinorbi <84071132+pfinorbi@users.noreply.github.com>
2023-11-11 16:38:50 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,12 @@
+function skillsMember() {
+  return {
+    restrict: 'E',
+    templateUrl: 'views/skills-member.html',
+    controller: 'SkillsMemberCtrl',
+    controllerAs: 'skillsMemberCtrl',
+    bindToController: true,
+    scope: {
+      member: '='
+    }
+  };
+}
